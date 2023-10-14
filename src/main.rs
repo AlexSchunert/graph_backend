920bfeb21d658ec2be423a66ce4f4dccab52f639
AlexSchunert <a.schunert@gmail.com>
2023-10-14 14:08:45 +0200

    added rust rocket backend hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+
+
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, Rocket!"
 }
+
+
+fn main() {
+    rocket::ignite().mount("/", routes![world]).launch();
+}
+
+/*
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+ */
+ 
+
+ /*
+ fn main() {
+
+    println!("Hello World");
+ }
+*/
