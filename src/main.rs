054408c1010a89df7e047e4b44ef6a5d96b35f48
AlexSchunert <a.schunert@gmail.com>
2023-10-14 20:04:33 +0200

    Added simple fileserver to deliver js and css files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
+
+
 #[macro_use]
 extern crate rocket;
+use std::path::{Path, PathBuf};
+use std::io;
+use rocket::{response::NamedFile, response::Responder};
 
-
-
-
+/* 
 #[get("/world")]
 fn world() -> &'static str {
     "Hello, Rocket!"
 }
+*/
 
+#[get("/")]
+fn index() ->  Option<NamedFile> {
+    NamedFile::open(Path::new("../../Frontend/graph_frontend/index.html")).ok()
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("../../Frontend/graph_frontend/").join(file)).ok()
+}
 
 fn main() {
-    rocket::ignite().mount("/", routes![world]).launch();
+    
+    rocket::ignite().mount("/", routes![index, files]).launch();
+    //rocket_instance.mount("../../Frontend/graph_frontend/index.html", File)
 }
 
 /*
